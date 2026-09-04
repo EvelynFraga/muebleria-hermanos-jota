c95c9e7f8a7f7124eabc6f4e0c188504dd33da6a
Evelyn <evlynfraga@gmail.com>
2026-09-04 20:04:38 -0300

    feat: add product data array

---
+++ app.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Aparador Uspallata",
+      descripcion: "Aparador de líneas contemporáneas para living o comedor.",
+      imagen: "imagenes/Aparador Uspallata.png",
+    },
+    {
+      id: 2,
+      nombre: "Sofá Patagonia",
+      descripcion: "Sofá de tres cuerpos para el living.",
+      imagen: "imagenes/Sofá Patagonia.png",
+    },
+    {
+      id: 3,
+      nombre: "Mesa Comedor Pampa",
+      descripcion: "Mesa de comedor de madera para reuniones en casa.",
+      imagen: "imagenes/Mesa Comedor Pampa.png",
+    },
+    {
+      id: 4,
+      nombre: "Sillón Copacabana",
+      descripcion: "Sillón de apoyo para living o rincón de lectura.",
+      imagen: "imagenes/Sillón Copacabana.png",
+    },
+  ];
